de205affc8a1a149ad6198afd8dad8306504ee76
zengts <905080195@qq.com>
2018-06-07 17:37:34 +0800

    个人中心

--- mpvue/my-project/src/App.vue
+++ mpvue/my-project/src/App.vue
@@ -1,31 +1,36 @@
-<script>
-export default {
-  created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+<template>
+<div>
 
-    console.log('app created and cache logs by setStorageSync')
-  }
+</div>
+</template>
+
+<script>
+import {get} from './util.js';
+import qcloud from 'wafer2-client-sdk';
+import config from './config.js';
+export default {
+async created(){
+// const res=await get('');
+var qcloud = require('./bower_components/qcloud-weapp-client-sdk/index.js');
+
+// 设置登录地址
+qcloud.setLoginUrl('https://199447.qcloud.la/login');
+qcloud.login({
+    success: function (userInfo) {
+        console.log('登录成功', userInfo);
+    },
+    fail: function (err) {
+        console.log('登录失败', err);
+    }
+});
+
+
+console.log(123,res);
+  console.log('小程序启动啦');
+}
 }
 </script>
 
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+
 </style>
---
+++ mpvue/my-project/src/config.js
@@ -0,0 +1,9 @@
+// 配置项
+
+const host ='http://192.168.1.80:3000/'
+
+const config={
+  host
+}
+
+export default config
